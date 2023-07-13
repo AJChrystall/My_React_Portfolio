173160c3b618914d638e3b2748fa263b2492ee75
Alexander Chrystall <zander@MacBook-Air-4.local>
2023-07-13 23:49:32 +1000

    please

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,32 +1,23 @@
 import React from 'react';
 
-function Header() {
-    return (
-      <header>
-        <h1>Developer's Name</h1>
-        <Navigation />
-      </header>
-    );
-  }
 function Resume({ currentSection }) {
-    const proficiencies = ['Proficiency 1', 'Proficiency 2', 'Proficiency 3'];
-  
-    return (
-      <section className={currentSection === 'Resume' ? 'selected' : ''}>
-        <h2>Resume</h2>
-        <a href="path/to/resume.pdf" download>
-          Download Resume
-        </a>
-  
-        <h3>Proficiencies</h3>
-        <ul>
-          {proficiencies.map((proficiency, index) => (
-            <li key={index}>{proficiency}</li>
-          ))}
-        </ul>
-      </section>
-    );
-  }
-  
+  const proficiencies = ['Proficiency 1', 'Proficiency 2', 'Proficiency 3'];
 
-export default Header;
+  return (
+    <section className={currentSection === 'Resume' ? 'selected' : ''}>
+      <h2>Resume</h2>
+      <a href="path/to/resume.pdf" download>
+        Download Resume
+      </a>
+
+      <h3>Proficiencies</h3>
+      <ul>
+        {proficiencies.map((proficiency, index) => (
+          <li key={index}>{proficiency}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Resume;
