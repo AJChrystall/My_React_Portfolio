41be02d5e86ca6809bce3d2e0c4913dcdd850d76
Alexander Chrystall <zander@MacBook-Air-4.local>
2023-07-13 23:59:56 +1000

    everything works

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -4,7 +4,7 @@ function AboutMe({ currentSection }) {
   return (
     <section className={currentSection === 'About Me' ? 'selected' : ''}>
       <img src="path/to/photo" alt="Developer" />
-      <p>Short bio about the developer.</p>
+      <p>Hello I am a new JavaScript Developer.</p>
     </section>
   );
 }
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navigation from './Navigation';
+import './style.css';
 
 function Header() {
   return (
     <header>
-      <h1>Developer's Name</h1>
-      <Navigation />
+      <h1>AJChrystall Portfolio</h1>
     </header>
   );
 }
 
 export default Header;
+
 
